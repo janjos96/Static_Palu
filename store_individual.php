f76eb9a61654d6bc14f969cc5089c332f621a9e4
janjos96 <joaoanjos_96@hotmail.com>
2018-09-12 17:59:33 +0100

    first image and description

--- store_individual.php
+++ store_individual.php
@@ -35,12 +35,12 @@
       <img class="logoimg" src="media/images/artigos/azul_surfboard/logo.png"/>
 
       <div class="description1">
-      <p class="description_title">Descrição                  </p>
-      <p class="description_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur lorem vestibulum risus posuere pretium. Quisque sed mattis nisl. Mauris nec molestie ipsum, ac molestie erat. Donec dapibus velit eget nisi blandit dictum. Suspendisse at placerat sapien. Fusce gravida massa vitae nisl accumsan, eget sodales sem tempus. Vestibulum a faucibus mi.</p>
-
-      
-    </div>
-
+        <p class="description_title">Descrição</p>
+        <p class="description_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur lorem vestibulum risus posuere pretium. Quisque sed mattis nisl. Mauris nec molestie ipsum, ac molestie erat. Donec dapibus velit eget nisi blandit dictum. Suspendisse at placerat sapien. Fusce gravida massa vitae nisl accumsan, eget sodales sem tempus. Vestibulum a faucibus mi.</p>
+      </div>
+      <div class="frontimg">
+        <img id="frontimg" src="media/images/artigos/azul_surfboard/front.png"/>
+      </div>
   </div>
 
 
